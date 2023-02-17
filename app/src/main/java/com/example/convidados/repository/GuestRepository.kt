f6ab5f1ba62c450ad158dda0d19cf3eec6ce6f03
HalliteCC <hallitecaie@gmail.com>
2023-02-17 16:44:25 -0300

    Putting context

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -1,6 +1,11 @@
 package com.example.convidados.repository
 
-class GuestRepository private constructor(){
+import android.content.Context
+
+class GuestRepository private constructor(context: Context){
+
+    private val guestDataBase = GuestDataBase(context)
+
 
     //Singleton
 
@@ -8,16 +13,19 @@ class GuestRepository private constructor(){
         private lateinit var repository: GuestRepository
 
 
-        fun getInstance(): GuestRepository {
+        fun getInstance(context: Context): GuestRepository {
             if (!Companion::repository.isInitialized) {
-                repository = GuestRepository()
+                repository = GuestRepository(context)
             }
             return repository
         }
     }
 
     fun save(){
-        GuestDataBase(null)
+    }
+
+    fun update(){
+
     }
 
 
--- app/src/main/java/com/example/convidados/viewmodel/GuestViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.convidados.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.convidados.repository.GuestRepository
 
-class GuestViewModel: ViewModel () {
+class GuestViewModel(application: Application): AndroidViewModel(application) {
 
     private val guestRepository = GuestRepository.getInstance()
 
