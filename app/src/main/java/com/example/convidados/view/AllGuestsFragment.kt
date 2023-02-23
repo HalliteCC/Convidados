85dab1d5f15130f86e3705705bc69e994e49515b
HalliteCC <hallitecaie@gmail.com>
2023-02-23 18:17:42 -0300

    Creating Layout / Adapter

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -1,4 +1,4 @@
- package com.example.convidados.view
+package com.example.convidados.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,38 +7,51 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.FragmentAllGuestsBinding
+import com.example.convidados.view.adapter.GuestsAdpater
 import com.example.convidados.viewmodel.AllGuestsViewModel
 
 
- class AllGuestsFragment : Fragment() {
+class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentAllGuestsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: AllGuestsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, b: Bundle?
     ): View {
-        val ViewModel =
-            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        ViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        //layout
+        binding.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
+
+        //adpter
+
+        binding.recyclerAllGuests.adapter = GuestsAdpater()
+
+        viewModel.getAll()
+
+        observe()
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observe() {
+        viewModel.guests.observe(viewLifecycleOwner) {
+            //Lista de convidados
+        }
     }
 }
